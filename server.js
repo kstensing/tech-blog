bd475a120d87c93e5f4b8ac22af30f688f1137ea
kstensing <stensingk@gmail.com>
2022-03-26 11:03:23 -0500

    added initial routes file structure and helper file

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
+const sequelize = require("./config/connection.js");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'Secrets',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -34,6 +34,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
