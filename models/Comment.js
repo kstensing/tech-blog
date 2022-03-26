99d7caac50f2eeadbc5462e9590a8339b6ab7a88
kstensing <stensingk@gmail.com>
2022-03-25 21:14:18 -0500

    created Comment model

---
+++ models/Comment.js
@@ -0,0 +1,54 @@
+const {
+    Model,
+    DataTypes
+} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    body: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1]
+        }
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false
+    },
+    updated_at: {
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        allowNull: false
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'post',
+            key: 'id'
+        }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    }
+}, {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+});
+
+module.exports = Comment;
---
+++ seeds/comment-seeds.js
@@ -0,0 +1,23 @@
+const { Comment } = require('../models');
+
+const commentdata = [
+  {
+    body: 'SLFJSLJ',
+    user_id: 1,
+    post_id: 1
+  },
+  {
+    body: 'sdhfjk',
+    user_id: 1,
+    post_id: 2
+  },
+  {
+    comment_text: 'sdfdv',
+    user_id: 1,
+    post_id: 3
+  }
+];
+
+const seedComments = () => Comment.bulkCreate(commentdata);
+
+module.exports = seedComments;
