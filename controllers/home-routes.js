56a71457f52dc6a294b166d2a2a586378a9f3ce3
kstensing <stensingk@gmail.com>
2022-03-27 22:22:17 -0500

    added if helper around comment form

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -71,7 +71,10 @@ router.get('/post/:id', (req, res) => {
                 return;
             }
             const post = postData.get({ plain: true });
-            res.render('single-post', { post });
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn
+              });
           })
           .catch(err => {
             console.log(err);
