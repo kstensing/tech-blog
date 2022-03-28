1566e499136ce798c6c31d268b0d138a0e06c113
kstensing <stensingk@gmail.com>
2022-03-27 22:00:26 -0500

    added single post template and route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -37,6 +37,48 @@ router.get('/', (req, res) => {
       });
 });
 
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'title',
+                'body',
+                'created_at',
+                'updated_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'body', 'created_at', 'updated_at', 'post_id', 'user_id'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(postData => {
+            if (!postData) {
+                res.status(404).json({
+                    message: 'There is no post found with that id!'
+                });
+                return;
+            }
+            const post = postData.get({ plain: true });
+            res.render('single-post', { post });
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+});
+
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
       res.redirect('/');
