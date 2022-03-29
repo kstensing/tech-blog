0782f926bd20c1ef7cafa54e85a1cd46a9d0848f
kstensing <stensingk@gmail.com>
2022-03-28 20:32:14 -0500

    added edit post template

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -8,45 +8,85 @@ const {
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, (req, res) => {
-    console.log("testing this route");
-Post.findAll({
-        where: {
-            user_id: req.session.user_id
-        },
-        attributes: [
-            'id',
-            'title',
-            'body',
-            'created_at',
-            'updated_at'
-        ],
-        include: [{
-                model: Comment,
-                attributes: ['id', 'body', 'created_at', 'updated_at', 'post_id', 'user_id'],
-                include: {
+    Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            attributes: [
+                'id',
+                'title',
+                'body',
+                'created_at',
+                'updated_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'body', 'created_at', 'updated_at', 'post_id', 'user_id'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
                     model: User,
                     attributes: ['username']
                 }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-    .then(postData => {
-        const posts = postData.map(post => post.get({
-            plain: true
-        }));
-        res.render('dashboard', {
-            posts,
-            loggedIn: true
+            ]
+        })
+        .then(postData => {
+            const posts = postData.map(post => post.get({
+                plain: true
+            }));
+            res.render('dashboard', {
+                posts,
+                loggedIn: true
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
         });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+});
+
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findByPk(req.params.id, {
+            attributes: [
+                'id',
+                'title',
+                'body',
+                'created_at',
+                'updated_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'body', 'created_at', 'updated_at', 'post_id', 'user_id'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(postData => {
+            if (postData) {
+                const post = postData.get({
+                    plain: true
+                });
+                res.render('edit-post', {
+                    post,
+                    loggedIn: true
+                });
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
